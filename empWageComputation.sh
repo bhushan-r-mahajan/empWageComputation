435a497dff7eb0ee3d6dd5ff0056ce1daeb88234
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-20 18:19:55 +0530

    Added calculation for dily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,16 @@
 echo "Welcome"
+
 attendance=$((RANDOM %2 ))
+wagePerHr=20
+fullDay=8
+
 if [ $attendance -eq 0 ]
 then
 	echo "Employee is Absent"
 elif [ $attendance -eq 1 ]
 then
 	echo "Employee is Present"
+	dailyWage=$(($wagePerHr * $fullDay ))
+	echo "Employee wage "$dailyWage
 fi
+
