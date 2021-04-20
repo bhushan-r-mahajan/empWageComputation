d10cfebf6549ca67ff987b921d9a2057b7b3e4a9
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-20 18:30:20 +0530

    Added part time calculations

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,22 @@
 echo "Welcome"
 
-attendance=$((RANDOM %2 ))
+attendance=$((RANDOM %3 ))
 wagePerHr=20
-fullDay=8
+fullTime=8
+partTime=4
 
 if [ $attendance -eq 0 ]
 then
 	echo "Employee is Absent"
 elif [ $attendance -eq 1 ]
 then
-	echo "Employee is Present"
-	dailyWage=$(($wagePerHr * $fullDay ))
-	echo "Employee wage "$dailyWage
+	echo "Employee is Full time"
+	dailyWage=$(($wagePerHr * $fullTime ))
+	echo "Employee wage is "$dailyWage
+elif [ $attendance -eq 2 ]
+then
+	echo "Employee is Part time"
+	dailyWage=$(( $wagePerHr * $partTime ))
+	echo "Employee wage is "$dailyWage
 fi
 
