18e196dd5826b9a085f1f5cf55c89356ecdd13de
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-20 20:10:59 +0530

    Added array to store daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,17 +1,17 @@
 echo "Welcome"
 
-wagePerHr=20
+wagePerHour=20
 totalWorkingDays=20
 maxWorkingHours=100
-
-
 fullTime=1
 partTime=2
+
 workingDays=0
 workingHours=0
 
 function workHours(){
- case $1 in
+
+	case $1 in
 
    $fullTime)
       empHours=8
@@ -24,15 +24,25 @@ function workHours(){
       ;;
    esac
 echo $empHours
+
 }
 
+function wagearray(){
+
+	local workHrs=$1
+	dailyWage=$(( $workHrs * $wagePerHour ))
+	echo $dailyWage
+
+}
 while [[ $workingDays -lt $totalWorkingDays && $workingHours -lt $maxWorkingHours ]]
 do
 	((workingDays++))
 	empHours="$( workHours $((RANDOM%3)))"
 	workingHours=$(( $workingHours + $empHours ))
+	empDailyWage[$workingDays]="$( wagearray $empHours)"
 done
 
-monthlyWage=$(($workingHours * $wagePerHr))
+monthlyWage=$(($workingHours * $wagePerHour))
 echo "Monthly Wage="$monthlyWage
 echo "Total time worked="$workingHours" Hrs"
+echo ${empDailyWage[@]}
