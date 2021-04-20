ce6b9871638949f423841418a155a95c2c48012e
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-20 19:06:59 +0530

    Added calculation for 20 days per month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,21 +1,39 @@
+
+
 echo "Welcome"
 
-attendance=$((RANDOM %3 ))
 wagePerHr=20
+totalWorkingDays=20
+monthlyWage=0
 fullTime=1
 partTime=2
+absent=0
 
-case $attendance in
+for (( day=1; day<=$totalWorkingDays; day++ ))
+do
 
-$fullTime)
-	echo "Employee is Full time"
-	empHours=8;;
-$partTime)
-	echo "Employee is Part time"
-	empHours=4;;
-*)
-   echo "Employee is Absent"
-	empHours=0;;
-esac
-dailyWage=$(( $wagePerHr * $empHours ))
-echo "Wage="$dailyWage
+	attendance=$((RANDOM%3))
+
+	case $attendance in
+
+	$fullTime)
+		echo $attendance
+		echo "Employee is Full time"
+		empHours=8
+		;;
+	$partTime)
+		echo $attendance
+		echo "Employee is Part time"
+		empHours=4
+		;;
+	$absent)
+		echo $attendance
+   	echo "Employee is Absent"
+		empHours=0
+		;;
+	esac
+	dailyWage=$(( $wagePerHr * $empHours ))
+	monthlyWage=$(($monthlyWage + $dailyWage))
+done
+
+echo "Monthly Wage="$monthlyWage
